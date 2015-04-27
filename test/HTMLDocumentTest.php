211c60ec131634d3bb6b0f297cb25fbb183e0882
Ben Cohen <ben.cohen@targetspot.com>
2015-04-27 15:44:07 -0400

    Added test cases

--- test/HTMLDocumentTest.php
+++ test/HTMLDocumentTest.php
@@ -1,5 +1,6 @@
 <?php
 include_once(__DIR__ . '/../vendor/autoload.php');
+include_once(__DIR__ . '/classes/EmptyTestDocument.php');
 include_once(__DIR__ . '/classes/TestDocument.php');
 include_once(__DIR__ . '/classes/TestDocumentClosures.php');
 include_once(__DIR__ . '/classes/TestComponent.php');
@@ -9,7 +10,7 @@ include_once(__DIR__ . '/classes/TestComponent.php');
  * Class HTMLDocumentTest
  * Test cases for testing a normal use case incorporating documents, elements, components, etc.
  */
-class HTMLDocumentTest extends PHPUnit_Framework_TestCase {
+class DocumentTest extends PHPUnit_Framework_TestCase {
 
 	public static $expectedHTML =
 <<<HTML
@@ -23,6 +24,43 @@ class HTMLDocumentTest extends PHPUnit_Framework_TestCase {
 </html>
 
 HTML;
+
+	/**
+	 * Test that the constructor for HTMLDocument returns an object
+	 */
+	public function testConstructor(){
+		$template = new EmptyTestDocument();
+		$this->assertInstanceOf('Tokamak\Dom\HTMLDocument', $template);
+	}
+
+	/**
+	 * Test that the HTMLDocument constructor correctly instantiates a DOMDocument
+	 */
+	public function testGetDom(){
+		$template = new EmptyTestDocument();
+		$dom = $template->getDom();
+		$this->assertInstanceOf('DOMDocument', $dom, 'Document constructor should create a DOMDocument instance.');
+	}
+
+	/**
+	 * Make sure that Document::append adds components and elements to the Dom
+	 */
+	public function testAppend(){
+		$template = new EmptyTestDocument();
+		$dom = $template->getDom();
+
+		// Check appending Component
+		$component = new TestComponent($dom, array("title" => "Example"));
+		$template->append($component);
+		$head = $dom->getElementsByTagName('head');
+		$this->assertEquals(1, $head->length, 'An appended component should be added to the underlying DOM.');
+
+		// Check appending Element
+		$element = new Tokamak\Dom\Element($dom, 'h1', null, 'Test');
+		$template->append($element);
+		$h1s = $dom->getElementsByTagName('h1');
+		$this->assertEquals(1, $h1s->length, 'an appended element should be added to the underlying DOM.');
+	}
 
 	/**
 	 * Test the output of an example Document class, including
---
+++ test/classes/EmptyTestDocument.php
@@ -0,0 +1,9 @@
+<?php
+include_once(__DIR__ . '/../../vendor/autoload.php');
+use Tokamak\Dom\HTMLDocument;
+
+class EmptyTestDocument extends  HTMLDocument {
+	public function render(){
+
+	}
+}
